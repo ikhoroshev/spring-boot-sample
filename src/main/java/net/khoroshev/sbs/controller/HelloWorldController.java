cacc05cd3f6defa1d58b49b9c085f3d68d61554c
Igor Khoroshev <igor@khoroshev.net>
2020-07-21 23:08:25 +0600

    Stage 2 prepare

--- src/main/java/net/khoroshev/sbs/controller/HelloWorldController.java
+++ src/main/java/net/khoroshev/sbs/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package net.khoroshev.sbs.controller;
 
+import net.khoroshev.sbs.service.GreetingService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,9 +8,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
+    private final GreetingService greetingService;
+
+    public HelloWorldController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     @GetMapping("/hello")
     @ResponseBody
     public String hello(@RequestParam String name, @RequestParam String lastName) {
-        return String.format("Hello %s %s!",name, lastName);
+        return greetingService.makeGreeting(name, lastName);
     }
 }
---
+++ src/main/java/net/khoroshev/sbs/service/GreetingService.java
@@ -0,0 +1,11 @@
+package net.khoroshev.sbs.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String makeGreeting(String name, String lastName) {
+        //TODO
+        return "TODO";
+    }
+}
