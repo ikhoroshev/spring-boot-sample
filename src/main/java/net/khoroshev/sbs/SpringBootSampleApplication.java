1092f79867a28ba09be965e97bd67a8366644c5d
Igor Khoroshev <igor@khoroshev.net>
2020-08-04 11:12:56 +0600

    repository

--- src/main/java/net/khoroshev/sbs/SpringBootSampleApplication.java
+++ src/main/java/net/khoroshev/sbs/SpringBootSampleApplication.java
@@ -2,8 +2,12 @@ package net.khoroshev.sbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@EnableJdbcRepositories
+@EnableJdbcAuditing
 public class SpringBootSampleApplication {
 
 	public static void main(String[] args) {
